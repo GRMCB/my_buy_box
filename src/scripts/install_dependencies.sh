2cccd026011644bed9ce25c2f0468d2b81a5ee01
GRMCB <grantmcbride89@gmail.com>
2023-08-02 22:11:21 -0700

    Changing from flask run to using gunicorn

--- src/scripts/install_dependencies.sh
+++ src/scripts/install_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo yum install python-pip -y
 pip3 install virtualenv
-pip3 install flask
-pip3 install gunicorn
 virtualenv -p python3 venv
 source venv/bin/activate
+pip3 install flask
+pip3 install gunicorn
 pip3 freeze > requirements.txt
 pip3 install -r requirements.txt
