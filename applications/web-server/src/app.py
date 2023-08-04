6f83536de64587b08b377a86a2ea84358dfefe98
GRMCB <grantmcbride89@gmail.com>
2023-08-03 21:52:59 -0700

    Restructured the html and route info

--- applications/web-server/src/app.py
+++ applications/web-server/src/app.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def home_page():
     return render_template("index.html")
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+@app.route("/verify", methods = ['POST', 'GET'])
+def verify():
+    if request.method == "POST":
+        zip_code = request.form["zip"]
+        zip_code_route = "/zipcode/" + str(zip_code)
+        return redirect(zip_code_route)
+
+@app.route("/zipcode/<zip_code>", methods = ['POST', 'GET'])
+def zip_code(zip_code):
+    return render_template('data.html', zip_code=zip_code)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
