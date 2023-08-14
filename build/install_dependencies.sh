58fe00fa858900ef262b38e418659803ba78f7b4
GRMCB <grantmcbride89@gmail.com>
2023-08-14 14:39:27 -0700

    Reinstalling dependencies for Python 3.9

--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo python3.9 -m ensurepip --upgrade
-pip3 install virtualenv
+python3.9 -m pip install virtualenv
 sudo virtualenv -p python3.9 venv
 source venv/bin/activate
 pip3 install -r requirements.txt
