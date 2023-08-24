a6295468bd54e995f74c3749ef0466d22f3655e2
GRMCB <grantmcbride89@gmail.com>
2023-08-23 22:36:33 -0700

    Adding database support to the data-collector app

--- applications/data-collector/src/main/app.py
+++ applications/data-collector/src/main/app.py
@@ -78,7 +78,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 client = Redfin()
 
-with app.app_context()
+with app.app_context():
     app.config['SQLALCHEMY_DATABASE_URI'] = \
         'sqlite:///' + os.path.join(basedir, 'database.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
