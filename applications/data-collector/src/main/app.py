f33b8068176d4d6b32ccf21e63355870ba4bc318
GRMCB <grantmcbride89@gmail.com>
2023-08-23 22:24:03 -0700

    Adding database support to the data-collector app

--- applications/data-collector/src/main/app.py
+++ applications/data-collector/src/main/app.py
@@ -8,7 +8,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from redfin import Redfin
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
-from components.listings.src.main.listing_record import ListingRecord
+# from components.listings.src.main.listing_record import ListingRecord
 from components.listings.src.main.listing_db import Database
 
 logging.basicConfig(level=logging.DEBUG,
@@ -16,6 +16,39 @@ logging.basicConfig(level=logging.DEBUG,
 
 logger = logging.getLogger(__name__)
 
+class ListingRecord(db.Model):
+    __tablename__ = 'listing_records'
+    id = db.Column(db.Integer, primary_key=True)
+    sale_type = db.Column(db.String(50))
+    sold_date = db.Column(db.String(50))
+    property_type = db.Column(db.String(50))
+    address = db.Column(db.String(100))
+    city = db.Column(db.String(50))
+    state_or_province = db.Column(db.String(50))
+    zip_or_postal_code = db.Column(db.String(50))
+    price = db.Column(db.Float)
+    beds = db.Column(db.Integer)
+    baths = db.Column(db.Float)
+    location = db.Column(db.String(50))
+    square_feet = db.Column(db.Integer)
+    lot_size = db.Column(db.Integer)
+    year_built = db.Column(db.Integer)
+    days_on_market = db.Column(db.Integer)
+    price_per_square_feet = db.Column(db.Float)
+    hoa_per_month = db.Column(db.Float)
+    status = db.Column(db.String(50))
+    next_open_house_start_time = db.Column(db.String(50))
+    next_open_house_end_time = db.Column(db.String(50))
+    url = db.Column(db.String(200))
+    source = db.Column(db.String(50))
+    mls_number = db.Column(db.Integer)
+    favorite = db.Column(db.String(1))
+    interested = db.Column(db.String(1))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Student {self.id}>'
 def load_user_listing_criteria():
     cur_path = os.path.dirname("listing_criteria.json")
     user_data_path = os.path.relpath('user_data/listing_criteria.json', cur_path)
@@ -83,7 +116,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = Database(app)
-
+db.create_all()
 
 if __name__ == '__main__':
     load_dotenv()
