eedb037284a2f90972fd041026d93ef5e68acb8b
GRMCB <grantmcbride89@gmail.com>
2023-08-24 21:35:49 -0700

    Adding database support to the data-collector app

--- applications/data-collector/src/main/app.py
+++ applications/data-collector/src/main/app.py
@@ -8,8 +8,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from redfin import Redfin
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
-# from components.listings.src.main.listing_record import ListingRecord
-# from components.listings.src.main.listing_db import Database
+from model import ListingRecord
 
 logging.basicConfig(level=logging.DEBUG,
                       format='%(asctime)s %(levelname)s %(message)s')
@@ -77,45 +76,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 client = Redfin()
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+db_path = os.path.join(parent_dir, 'database', 'database.db')
 
 with app.app_context():
-    app.config['SQLALCHEMY_DATABASE_URI'] = \
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
     db = SQLAlchemy(app)
     db.create_all()
-
-class ListingRecord(db.Model):
-    __tablename__ = 'listing_records'
-    id = db.Column(db.Integer, primary_key=True)
-    sale_type = db.Column(db.String(50))
-    sold_date = db.Column(db.String(50))
-    property_type = db.Column(db.String(50))
-    address = db.Column(db.String(100))
-    city = db.Column(db.String(50))
-    state_or_province = db.Column(db.String(50))
-    zip_or_postal_code = db.Column(db.String(50))
-    price = db.Column(db.Float)
-    beds = db.Column(db.Integer)
-    baths = db.Column(db.Float)
-    location = db.Column(db.String(50))
-    square_feet = db.Column(db.Integer)
-    lot_size = db.Column(db.Integer)
-    year_built = db.Column(db.Integer)
-    days_on_market = db.Column(db.Integer)
-    price_per_square_feet = db.Column(db.Float)
-    hoa_per_month = db.Column(db.Float)
-    status = db.Column(db.String(50))
-    next_open_house_start_time = db.Column(db.String(50))
-    next_open_house_end_time = db.Column(db.String(50))
-    url = db.Column(db.String(200))
-    source = db.Column(db.String(50))
-    mls_number = db.Column(db.Integer)
-    favorite = db.Column(db.String(1))
-    interested = db.Column(db.String(1))
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
 
     def __repr__(self):
         return f'<Student {self.id}>'
---
+++ applications/data-collector/src/main/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class ListingRecord(Base):
+    __tablename__ = 'listing_records'
+    id = Column(Integer, primary_key=True)
+    sale_type = Column(String(50))
+    sold_date = Column(String(50))
+    property_type = Column(String(50))
+    address = Column(String(100))
+    city = Column(String(50))
+    state_or_province = Column(String(50))
+    zip_or_postal_code = Column(String(50))
+    price = Column(Float)
+    beds = Column(Integer)
+    baths = Column(Float)
+    location = Column(String(50))
+    square_feet = Column(Integer)
+    lot_size = Column(Integer)
+    year_built = Column(Integer)
+    days_on_market = Column(Integer)
+    price_per_square_feet = Column(Float)
+    hoa_per_month = Column(Float)
+    status = Column(String(50))
+    next_open_house_start_time = Column(String(50))
+    next_open_house_end_time = Column(String(50))
+    url = Column(String(200))
+    source = Column(String(50))
+    mls_number = Column(Integer)
+    favorite = Column(String(1))
+    interested = Column(String(1))
+    latitude = Column(Float)
+    longitude = Column(Float)
+
+    def __repr__(self):
+        return f'<ListingRecord {self.id}>'
