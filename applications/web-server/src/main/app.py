6c58c08b83b52d75507790c2239926ba415cb1dc
GRMCB <grantmcbride89@gmail.com>
2023-08-27 13:57:31 -0700

    Update listingRecord object to strings

--- applications/web-server/src/main/app.py
+++ applications/web-server/src/main/app.py
@@ -27,7 +27,7 @@ def verify():
 @app.route("/zipcode/<zip_code>", methods = ['POST', 'GET'])
 def zip_code(zip_code):
     # Call Database Rest API to get Zip code listings
-    listings = requests.get(f"http://127.0.0.1:8081/api/listing/{zip_code}")
+    listings = requests.get(f"http://127.0.0.1:8081/api/listings/{zip_code}")
 
     return render_template('data.html', listings=listings)
 
