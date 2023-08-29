ce9f16e52206f4ef34888e50b43a823970f2dcec
GRMCB <grantmcbride89@gmail.com>
2023-08-29 16:21:22 -0700

    Refactoring app data-collector app to app factory design pattern

--- applications/data-collector/src/main/config.py
+++ applications/data-collector/src/main/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     db_path = os.path.join(os.path.dirname(__file__), 'database', 'database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = ="sqlite:///"+db_path
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"+db_path
