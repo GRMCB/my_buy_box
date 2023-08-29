54049fcbbc6644b9c5fde2d610581293dd8647c2
GRMCB <grantmcbride89@gmail.com>
2023-08-29 16:27:12 -0700

    Refactoring app data-collector app to app factory design pattern

--- applications/data-collector/src/main/app.py
+++ applications/data-collector/src/main/app.py
@@ -79,7 +79,10 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(func=get_all_user_listings, trigger="interval", seconds=10)
 scheduler.start()
 
-db.create_all()
+with app.app_context():
+
+    logger.info("Running with app.app_context():")
+    db.create_all()
 
 @app.route("/api/listings/<zip_code>", methods = ['GET'])
 def get_listings(zip_code):
