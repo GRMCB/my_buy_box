f2fa8ab2473f41e9832e7467cb13635392e57157
GRMCB <grantmcbride89@gmail.com>
2023-10-01 18:30:23 +0000

    Placed consumer in separate thread

--- applications/data-analyzer/src/main/app.py
+++ applications/data-analyzer/src/main/app.py
@@ -12,7 +12,6 @@ from flask import Flask, render_template
 from config import Config
 import requests
 # from bs4 import BeautifulSoup
-from consumer import consume
 from threading import Thread
 
 logging.basicConfig(level=logging.DEBUG,
@@ -29,6 +28,24 @@ def create_app(config_obj=Config):
     db.init_app(app)
 
     return app
+
+def callback(ch, method, properties, body):
+    body = json.loads(body)
+    print(" [x] Received %r" % body)
+    analyze_all_listings()
+
+def consume():
+    # Message Queue
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', heartbeat=36000))
+    channel = connection.channel()
+    channel.basic_qos(prefetch_count=1)
+
+    channel.queue_declare(queue='analyze')
+    channel.basic_consume(queue='analyze', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+    # End of Message Queue code
 
 def get_all_listings_from_collector_database():
 
