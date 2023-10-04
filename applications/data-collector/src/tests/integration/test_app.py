e6121c43569eadd44d7be411110fa578665460e0
GRMCB <grantmcbride89@gmail.com>
2023-10-04 23:22:10 +0000

    Added /health API healthcheck Integration test

--- applications/data-collector/src/tests/integration/test_app.py
+++ applications/data-collector/src/tests/integration/test_app.py
@@ -1,5 +1,11 @@
+from unittest.mock import patch
+
 def test_healthcheck(app_test_client):
-    """ This test tests whether the /health endpoint returns Healthy"""
-    response = app_test_client.get('/health')
+    with patch(app_test_client.get) as mock_query:
+        # Set up the mock object to return the desired data
+        mock_query.return_value = [(1,)]
+
+        """ This test tests whether the /health endpoint returns Healthy"""
+        response = mock_query('/health')
 
     assert "Data Collector app is Healthy" in response.text
